dfb90771b8231a8801430eeeba070d6418883150
Sujon Pal <dev.sujonpal@gmail.com>
2026-01-30 20:47:59 +0600

    interviewQuession

---
+++ 04-API-JSON-DataLoad-DynaicWebsite/05-async.js
@@ -0,0 +1,9 @@
+const handleAsync = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("Error:" , error)
+  }
+};
